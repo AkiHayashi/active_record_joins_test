be8dd1f90594aa698f6de006b6c01f2c36d1b3ba
aki hayashi <hayashi1019toy@gmail.com>
2022-10-08 18:56:49 +0900

    fix: 🐛 exercise2

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -9,7 +9,7 @@ class ExercisesController < ApplicationController
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop.left_outer_joins(foods: :orders).where(orders: {id: nil})
+    @shops = Shop.left_outer_joins(foods: :orders).where(orders: {id: nil}).group(:id)
   end
 
   def exercise3 
